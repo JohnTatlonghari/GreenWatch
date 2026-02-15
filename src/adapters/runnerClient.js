fb9005fdd792455702169aef3670617bbb515469
katsaliya <kataliyaschool@gmail.com>
2026-02-14 18:21:55 -0800

    Temporary runnerClient to test orchestrator before integrating Executorch

---
+++ src/adapters/runnerClient.js
@@ -0,0 +1,8 @@
+// Temporary runner stub
+// This simulates model behavior until Dev B integrates ExecuTorch
+
+export async function generateReply({ messages, slots }) {
+
+  // Simple simulated response
+  return `Understood. You are performing a ${slots.taskType} task and it is ${slots.urgency}. Here are the recommended next steps.`;
+}
